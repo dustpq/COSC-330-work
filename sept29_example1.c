0da0ae1348b712b64544ce39c4955e9b4c493175
dust.pq <portillo0121@gmail.com>
2025-09-29 10:22:18 -0400

    added comment notes to the source code

--- sept29_example1.c
+++ sept29_example1.c
@@ -7,6 +7,8 @@ int main() {
   // A null terminated array of character
   // pointers
   char *args[] = {"./sept29_example1_hello", NULL};
+  // changed ./hello to ./sept29_example1_hello.c so it will actually exec the
+  // file
   execvp(args[0], args);
 
   /*All statements are ignored after execvp() call as this whole
--- sept29_example2.c
+++ sept29_example2.c
@@ -8,12 +8,16 @@ int main() {
     /* was not successfully */
   } else if (pid > 0) {
     /* Parent process */
+    printf("Parent PID: %i\n", getpid());
   } else {
     /* Child process */
-    for (int i = 0; i < 5; i++) {
-      fork();
+    for (int i = 0; i < 3; i++) {
+      // You can see here if we print the child's PID, you can see that each
+      // time the number is printed, the PID is displayed, and it is distinct
+      // from the parent's PID
+      printf("Child PID: %i\n", getpid());
       printf("%i\n", i);
-      usleep(1000);
+      usleep(2);
     }
     exit(0);
   }
